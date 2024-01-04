89cd9ae2d6f310bc7a27ec1998d4dcc025f8e8bc
Jacob Brown <brown.jacob.alexander@gmail.com>
2024-01-04 09:33:57 -0700

    Added classes for NMBGMR descriptions and summaries

---
+++ routers/location/metadata.py
@@ -0,0 +1,21 @@
+class NMBGMRSummary():
+
+    def __init__(self):
+        self.all = ""
+        self.equipment = ""
+        self.notes = ""
+        self.projects = ""
+        self.owners = ""
+        self.photos = ""
+        self.photo_photoid = ""
+
+class NMBGMRDescription(object):
+
+    def __init__(self):
+        self.all = ""
+        self.equipment = ""
+        self.notes = ""
+        self.projects = ""
+        self.owners = ""
+        self.photos = ""
+        self.photo_photoid = ""
