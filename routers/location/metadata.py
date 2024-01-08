538abf0418ef7c9e68ca8803ada21c5da37bcaa9
Jacob Brown <brown.jacob.alexander@gmail.com>
2024-01-08 16:24:03 -0700

    Update /public/locations summaries and descriptions
    Clarified GeoJSON returns
    Pointed users to the schema to determine which attributes are available
        for location info and wells

--- routers/location/metadata.py
+++ routers/location/metadata.py
@@ -5,166 +5,134 @@
 class PublicSummary():
 
     def __init__(self):
-        self.all = "Get all publicly available well locations as a list of geojsons-formatted dictionaries"
-        self.collaborative_network = "Get collaborative network locations as a list of geojsons-formatted dictionaries"
-        self.usgs_site_metadata = "Get USGS site metadata"
+        self.all = "Get all publicly available locations"
+        self.collaborative_network = "Get all Healy Collaborative Network locations"
+        self.usgs_site_metadata = "Get USGS site metadata for a specific location"
         self.info = "Get information about a specific location"
-        self.well = "Get well and corresponding location information for a specific well"
+        self.well = "Get well information for a specific location"
 
 class PublicDescription():
 
     def __init__(self):
         self.all = """
-        Returns a list of dictionaries that contain information about publicly
-        available well locations and associated data. Each location must contain
-        a northing and an easting. 
+Returns data on all locations as a collection of
+<a href="https://geojson.org/" target="_blank">GeoJSONs</a> stored in an array: 
 
-        Each dictionary is formatted as a geojson as follows:
+```        
+{
+    "type": "FeatureCollection:
+    "features": [<Location GeoJSON>...]
+}
+```
 
-        {
-            "type": "Feature",
-            "properties": "{
-                "name": <PointID>,
-                "well_depth": {"value": <well depth>, "units": "ft"}
-            },
-            "geometry": {
-                "coordinates": [<longitude>, <latitude>, <elevation or 0>],
-                "type": "Point
-            }
-        }
-        """
+Each Location GeoJSON is formatted as follows:
+
+```
+{
+    "type": "Feature",
+    "properties": "{
+        "name": <PointID>,
+        "well_depth": {"value": <well depth>, "units": "ft"}
+    },
+    "geometry": {
+        "coordinates": [<longitude>, <latitude>, <elevation or 0>],
+        "type": "Point
+    }
+}
+```
+"""
 
         self.collaborative_network = """
-        Returns a list of dictionaries that contain information about
-        collaborative network well locations and associated data. Each location
-        must contain a northing and an easting. 
+Returns data on locations that are in the Healy Collaboratieve Network as a 
+collection of <a href="https://geojson.org/" target="_blank">GeoJSONs</a> 
+stored in an array: 
 
-        Each dictionary is formatted as a geojson as follows:
+```        
+{
+    "type": "FeatureCollection:
+    "features": [<Location GeoJSON>...]
+}
+```
 
-        {
-            "type": "Feature",
-            "properties": "{
-                "name": <PointID>,
-                "well_depth": {"value": <well depth>, "units": "ft"}
-            },
-            "geometry": {
-                "coordinates": [<longitude>, <latitude>, <elevation or 0>],
-                "type": "Point
-            }
-        }
-        """
+Each Location GeoJSON is formatted as follows:
+
+```
+{
+    "type": "Feature",
+    "properties": "{
+        "name": <PointID>,
+        "well_depth": {"value": <well depth>, "units": "ft"}
+    },
+    "geometry": {
+        "coordinates": [<longitude>, <latitude>, <elevation or 0>],
+        "type": "Point
+    }
+}
+```
+"""
 
         self.usgs_site_metadata = """
-        Returns a dictionary of metadata from a USGS site. The data is retrieved
-        from the NWIS service <a href="https://waterservices.usgs.gov/rest/Site-Service.html">https://waterservices.usgs.gov/rest/Site-Service.html</a>'.
-        If the metadata  cannot be retrieved or there is an error code as a 
-        result of the request to USGS, returns None.
+Returns a JSON of site metadata from a USGS site. The data is retrieved
+from the <a href="https://waterservices.usgs.gov/docs/" target="_blank">USGS Water Services</a>.
+If the metadata  cannot be retrieved or there is an error code as a 
+result of the request to USGS, returns None.
 
-        The dictionary has the following keys
+The dictionary has the following keys. See <a href="https://help.waterdata.usgs.gov/codes-and-parameters/codes" target="_blank">documentation</a> from the USGS for
+further explanations. 
 
-        - **agency_cd**: agency
-        - **site_no**: site identification number
-        - **station_nm**: site name
-        - **site_tp_cd**: site type
-        - **lat_va**: DMS latitude
-        - **long_va**: DMS longitude
-        - **dec_lat_va**: decimal latitude
-        - **dev_long_va**: decimal longitude
-        - **coord_meth_cd**: latitude-longitude method
-        - **coord_acy_cd**: latitude-longitude accuracy
-        - **coord_datum_cd**: latitude-longitude datum
-        - **dec_coord_datum_cd**: decomal latitude-longitude datum
-        - **district_cd**: district code
-        - **state_cd**: state code
-        - **county_cd**: county code
-        - **country_cd**: country code
-        - **land_net_ds**: land net location description
-        - **map_nm**: name of location map
-        - **map_scale_fc**: scale of location map
-        - **alt_va**: altitude accuracy
-        - **alt_meth_cd**: method altitude determined
-        - **alt_acy_va**: altitude accuracy
-        - **alt_datum_cd**: altitude datum
-        - **huc_cd**: hydrologic unit code
-        - **basin_cd**: drainage basin code
-        - **topo_cd**: topographic setting code
-        - **instruments_cd**: flags for instruments at site
-        - **construction_dt**: date of first construction
-        - **inventory_dt**: date site established or inventoried
-        - **drain_area_va**: drainage area
-        - **contrib_drain_area_va**: contributing drainage area
-        - **tz_cd**: time zon abbreviation
-        - **local_time_fg**: site honors Daylight Savings Time
-        - **reliability_cd**: data reliability code
-        - **gw_file_cd**: data-other GW files
-        - **nat_aqfr_cd**: national aquifer code
-        - **aqfr_cd**: local aquifer code
-        - **aqfr_type_cd**: local aquifer type code
-        - **well_depth_va**: well depth
-        - **hole_depth_va**: hole depth
-        - **depth_src_cd**: source of depth data
-        - **project_no**: project number
-        """
+- **agency_cd**: agency
+- **site_no**: site identification number
+- **station_nm**: site name
+- **site_tp_cd**: site type
+- **lat_va**: DMS latitude
+- **long_va**: DMS longitude
+- **dec_lat_va**: decimal latitude
+- **dev_long_va**: decimal longitude
+- **coord_meth_cd**: latitude-longitude method
+- **coord_acy_cd**: latitude-longitude accuracy
+- **coord_datum_cd**: latitude-longitude datum
+- **dec_coord_datum_cd**: decomal latitude-longitude datum
+- **district_cd**: district code
+- **state_cd**: state code
+- **county_cd**: county code
+- **country_cd**: country code
+- **land_net_ds**: land net location description
+- **map_nm**: name of location map
+- **map_scale_fc**: scale of location map
+- **alt_va**: altitude accuracy
+- **alt_meth_cd**: method altitude determined
+- **alt_acy_va**: altitude accuracy
+- **alt_datum_cd**: altitude datum
+- **huc_cd**: hydrologic unit code
+- **basin_cd**: drainage basin code
+- **topo_cd**: topographic setting code
+- **instruments_cd**: flags for instruments at site
+- **construction_dt**: date of first construction
+- **inventory_dt**: date site established or inventoried
+- **drain_area_va**: drainage area
+- **contrib_drain_area_va**: contributing drainage area
+- **tz_cd**: time zon abbreviation
+- **local_time_fg**: site honors Daylight Savings Time
+- **reliability_cd**: data reliability code
+- **gw_file_cd**: data-other GW files
+- **nat_aqfr_cd**: national aquifer code
+- **aqfr_cd**: local aquifer code
+- **aqfr_type_cd**: local aquifer type code
+- **well_depth_va**: well depth
+- **hole_depth_va**: hole depth
+- **depth_src_cd**: source of depth data
+- **project_no**: project number
+"""
 
         self.info = """
-        Returns location information for a specific PointID. The Location object
-        being returned has the following attributes:
-
-        - **LocationId**: primary key
-        - **PointID**
-        - **SiteID**
-        - **AlternateSiteID**
-        - **SiteNames**
-        - **PublicRelease**
-        - **Easting**
-        - **Northing**
-        - **Altitude**
-        - **LocationNotes**
-        - **AltitudeMethod**
-        - **lu_elevation_method**
-        - **Elevation**
-        - **elevation_method**
-        - **geometry**
-        - **lonlat**
-        """
+Returns a JSON containing Location information for a specific **PointID**. See the
+Location schema for the keys.
+"""
 
         self.well = """
-        Returns well and associated location information for a specific PointID.
-        The object being returned has the following attributes:
-
-        - **LocationId**: used to join well information to location information
-        - **PointID**
-        - **WellID**
-        - **HoleDepth**
-        - **WellDepth**
-        - **OSEWellID**
-        - **OSEWelltagID**
-        - **MeasuringPoint**
-        - **MPHeight**
-        - **CasingDiameter**
-        - **CasingDepth**
-        - **CasingDescription**
-        - **FormationZone**
-        - **StaticWater**
-        - **DataSource**
-        - **MonitoringStatus**
-        - **pod_url**
-        - **formation**
-        - **SiteID**
-        - **AlternateSiteID**
-        - **SiteNames**
-        - **PublicRelease**
-        - **Easting**
-        - **Northing**
-        - **Altitude**
-        - **LocationNotes**
-        - **AltitudeMethod**
-        - **lu_elevation_method**
-        - **Elevation**
-        - **elevation_method**
-        - **geometry**
-        - **lonlat**
-        """
+Returns a JSON containing well information for a specific **PointID**. See the 
+Well schema for the keys.
+"""
 
 # ==============================================================================
 # END PUBLIC ENDPOINT METADATA CLASSES
